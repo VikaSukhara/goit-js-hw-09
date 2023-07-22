1398870dda0a5974fb2926b6bc1bac84997ed2d7
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-07-22 15:17:44 +0200

    add-const-refs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,6 @@
 import Notiflix from 'notiflix';
 
-refs = {
+const refs = {
   form: document.querySelector('form'),
   amountEl: document.querySelector('[name="amount"]'),
   firstDelay: document.querySelector('[name="delay"]'),
